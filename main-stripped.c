59895ad22e119f0577e7fd5ef696c83ea2cd03bc
Skunkdog <gdascota@gmail.com>
2026-06-12 02:24:28 +0300

    Added playlist

--- main-stripped.c
+++ main-stripped.c
@@ -1,44 +1,75 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <dirent.h>
+#include <string.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     // Initialization
   
     const int WIDTH = 400;
     const int HEIGHT = 350;
     const int FPS = 60;
-    int CenterButtonState = 0;
-    
-    SetTargetFPS(FPS);
-    
-    InitWindow(WIDTH, HEIGHT, "Openpiece-stripped");
-        InitAudioDevice();
 
-    if (IsAudioDeviceReady() == 0  || IsWindowReady() == 0){
-      return 1;
-	}
-    float MasterVolume = GetMasterVolume();
-    Sound Music = LoadSound(argv[1]);
-    if (IsSoundValid(Music) == 0){
-      printf("\033[31mSomething wrong with loading file.\nCheck the directory name.\n\033[0m");
+    const char *path = (argc > 1) ? argv[1] : ".";
+    ChangeDirectory(path);
+    DIR *dir = opendir(path);
+    if (!dir){
+      perror("opendir");
       return 1;
     }
+    printf("%s\n", path);
+    struct dirent *c_entry;
+    struct dirent *entry;
+    int count = 0;
+    while ( (c_entry = readdir(dir)) != NULL){
+	count++;
+      }
+    closedir(dir);
+    DIR *dir2 = opendir(path);
+    readdir(dir2);
+    readdir(dir2);
+    char *playlist[count] = {};
+    for (int i = 0; i < count; i++){
+      entry = readdir(dir2);
+      if (entry == NULL){
+	break;
+      }
+      playlist[i] = entry->d_name;
+    }
+    
+    
+    SetTraceLogLevel(LOG_NONE);
+    InitWindow(WIDTH, HEIGHT, "Openpiece-stripped"); //Initialise window
+    InitAudioDevice(); //Initialise audio device
+    
+    SetTargetFPS(FPS); //Sets max FPS
+    int CenterButtonState = 0;
+    if (IsWindowReady() == 0){  //Checks window works properly
+      perror("IsWindowReady");
+      return 1;
+    }
+    if (IsAudioDeviceReady() == 0){ //Checks audio device works properly
+      perror("IsAudioDeviceReady");
+      return 1;
+    }
+
+    printf("After Checks\n");
+    Sound Music = LoadSound(playlist[3]);
     PlaySound(Music);
-    if (IsSoundPlaying(Music) == 1){
-      printf("\033[31mPlaying music.\n\033[0m");
-    }
+    
     
     // Main loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-      SetExitKey(81);    
-      SetWindowState(0x00000004);
-      BeginDrawing();
+           SetExitKey(81);    // Set exit key to q
+      SetWindowState(0x00000004); //Make window movable
 
-      ClearBackground(BLACK);
-      
-      if (IsKeyPressed(32) == 1)
+      BeginDrawing(); //Start drawing part
+
+      ClearBackground(BLACK); //Set black background
+
+      if (IsKeyPressed(32) == 1) // If space pressed cycle through pause and unpause
 	{
 	  CenterButtonState++;
 	  if (CenterButtonState > 1)
@@ -47,12 +78,13 @@ int main(int argc, char* argv[])
 	    }
 	}
       if (CenterButtonState == 1){	
-	PauseSound(Music);
+	//PauseSound(Music);
       }
       else{
-	ResumeSound(Music);
+	//	ResumeSound(Music);
       }
-      EndDrawing();
+      
+      EndDrawing(); //Stops drawing 
     }
     
     CloseWindow();
